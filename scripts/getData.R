4e20b35fa17210524a257622af022b6456fe4271
Max Blasdel <maxblasdel@gmail.com>
2020-07-02 07:09:50 -0700

    init

---
+++ scripts/getData.R
@@ -0,0 +1,83 @@
+
+# get data from ESRI server for tree map
+library(sf)
+library(dplyr)
+library(ggplot2)
+# load data ---------------------------
+
+# Street Tree Data
+trees <- read_sf('https://opendata.arcgis.com/datasets/eb67a0ad684d4bb6afda51dc065d1664_25.geojson')
+
+# Park tree data
+park_tree <- read_sf("https://opendata.arcgis.com/datasets/83a7e3f6a1d94db09f24849ee90f4be2_220.geojson")
+
+# neighborhoods
+neighborhoods <- read_sf("https://opendata.arcgis.com/datasets/1ef75e34b8504ab9b14bef0c26cade2c_3.geojson")
+
+# parks 
+parks <- read_sf("https://opendata.arcgis.com/datasets/9eef54196eaa4d12b54e9bc40e70ff09_35.geojson")
+
+# Clean Data -------------------------------------
+
+# drop unneeded columns and output in geopackage format
+
+# clean parks
+parks <- 
+  parks %>% 
+  select(NAME, ACRES)
+
+# clean neighborhoods
+neighborhoods <-
+  neighborhoods %>% 
+  select(MAPLABEL)
+
+# clean street trees
+# this is the largest data set
+trees <- 
+  trees %>% 
+  select(Family, Genus, Common, DBH, Edible, Neighborhood)
+
+# park trees
+park_tree <- 
+  park_tree %>% 
+  select(Family, Genus, Common_name, Total_Annual_Benefits, DBH, Edible, Native)
+
+# Analysis
+
+# what am i trying to answer/show here?
+# which neighborhoods have the most trees per area?
+tr_nei <- st_intersection(trees, neighborhoods)
+
+tr_nei %>% 
+  filter(is.na(Neighborhood))
+# calculate number of trees in each neighborhood
+# neighborhoods <- 
+n_trees  <- 
+  tr_nei %>% 
+  group_by(MAPLABEL) %>% 
+  summarise(n_trees = n()) %>% 
+  as.data.frame()
+
+st_as_sf(n_trees) %>% 
+ggplot() +
+  geom_sf()
+  
+# join n trees together
+neighborhoods <- left_join(neighborhoods, n_trees, by = "MAPLABEL")
+
+neighborhoods %>% 
+  mutate(trees_per_acre = st_area(.))
+
+
+# what are the most valauble parks? Both in gross value and per acre?
+# what neighborhoods have the most edible trees?
+# parks with the msot edible/native trees?
+
+
+
+
+# Output Data -------------------------------------------------------------
+
+write_sf()
+
+
