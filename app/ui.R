953ba5c89586f33a765cb2e1abb535f62181db53
mxblsdl <maxblasdel@gmail.com>
2020-07-05 15:35:53 -0700

    move app files

--- app/ui.R
+++ app/ui.R
@@ -1,11 +1,8 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
+# tree map app for Portland
+### Author: Max Blasdel
+### Contact: maxblasdel@gmail.com
+
 
 library(shiny)
 
