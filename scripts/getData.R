e43a20921a0c0ec119bb392ea590527e115e5b2a
mxblsdl <maxblasdel@gmail.com>
2021-02-02 19:40:00 -0800

    improvements to data cleaning of neighborhoods

--- scripts/getData.R
+++ scripts/getData.R
@@ -17,7 +17,8 @@ trees <- read_sf('https://opendata.arcgis.com/datasets/eb67a0ad684d4bb6afda51dc0
 park_tree <- read_sf("https://opendata.arcgis.com/datasets/83a7e3f6a1d94db09f24849ee90f4be2_220.geojson")
 
 # neighborhoods
-neighborhoods <- read_sf("https://opendata.arcgis.com/datasets/1ef75e34b8504ab9b14bef0c26cade2c_3.geojson")
+# neighborhoods <- read_sf("https://opendata.arcgis.com/datasets/1ef75e34b8504ab9b14bef0c26cade2c_3.geojson")
+neighborhoods <- read_sf("https://opendata.arcgis.com/datasets/9f50a605cf4945259b983fa35c993fe9_125.geojson")
 
 # parks 
 parks <- read_sf("https://opendata.arcgis.com/datasets/9eef54196eaa4d12b54e9bc40e70ff09_35.geojson")
@@ -36,10 +37,21 @@ parks <-
   parks %>% 
   select(NAME, ACRES)
 
-# clean neighborhoods
-neighborhoods <-
-  neighborhoods %>% 
-  select(MAPLABEL)
+# Combining polygons with two neighborhood names
+# Simply taking first rather doing indiv investigation
+# Remove any long name titles
+neigh <- neighborhoods %>% 
+  mutate(MAPLABEL = gsub("/.*", "", MAPLABEL),
+         MAPLABEL = gsub("Association", "Assn.", MAPLABEL),
+         MAPLABEL = gsub("Community Assn.", "", MAPLABEL),
+         MAPLABEL = gsub("Assn. of Neighbors", "", MAPLABEL),
+         MAPLABEL = gsub("Neighborhood District Assn.|District Assn.", "", MAPLABEL),
+         MAPLABEL = gsub("Improvement League|Residential League|Neighborhood Network|Foothills League",
+                         "",
+                         MAPLABEL)
+         ) %>% 
+  group_by(MAPLABEL) %>% 
+  summarise()
 
 # clean street trees
 # this is the largest data set
@@ -56,7 +68,7 @@ park_tree <-
 
 ## Neighborhoods -----------------------------------------------------------------------
 # which neighborhoods have the most trees per area?
-tr_nei <- st_join(trees, neighborhoods)
+tr_nei <- st_join(trees, neigh)
 
 # calculate number of trees in each neighborhood
 neighborhoods <- 
@@ -64,9 +76,10 @@ neighborhoods <-
   st_set_geometry(NULL) %>% 
   group_by(MAPLABEL) %>% 
   summarise(n_trees = n()) %>% 
-  left_join(neighborhoods, ., by = "MAPLABEL") %>% 
-  mutate(trees_per_acre = n_trees / st_area(.),
-         trees_per_acre = set_units(trees_per_acre, acre))
+  left_join(neigh, ., by = "MAPLABEL") %>% 
+  mutate(acres = st_area(.),
+         acres = set_units(acres, acre),
+         trees_per_acre = n_trees / acres)
 
 # what neighborhoods have the most edible trees?
 neighborhoods <- 
@@ -82,15 +95,27 @@ neighborhoods <-
 # Parks -------------------------------------------------------------------
 
 # what are the most valuable parks? Both in gross value and per acre?
+parks %>% 
+  mutate(acres = st_area(.),
+         acres = set_units(acres, acre))
+
 tr_par <- st_join(parks, park_tree)
+
+# calculate the total number of trees in each park
+parks <-
+  tr_par %>% 
+  st_set_geometry(NULL) %>% 
+  group_by(NAME) %>% 
+  summarise(total_trees = n()) %>% 
+  left_join(parks, ., by = "NAME")
 
 parks <- 
   tr_par %>% 
   st_set_geometry(NULL) %>% 
   group_by(NAME) %>% 
-  summarise(val = sum(Total_Annual_Benefits, na.rm = T)) %>% 
+  summarise(total_annual_benefits = sum(Total_Annual_Benefits, na.rm = T)) %>% 
   left_join(parks, ., by = "NAME") %>% 
-  mutate(val_per_acre = val / ACRES)
+  mutate(benefits_per_acre = total_annual_benefits / ACRES)
 
 # parks with the most edible/native trees?
 parks <- 
@@ -102,21 +127,31 @@ parks <-
   pivot_wider(values_from = "n_fruit", names_from = "Edible") %>% 
   right_join(., parks, by = "NAME")
 
+# Drop the unclaimed areas
+neighborhoods <- neighborhoods %>% 
+  filter(! grepl("Unclaimed", MAPLABEL))
+
+# replace an NA values with 0
+parks[is.na(parks)] <- 0
+neighborhoods[is.na(neighborhoods)] <- 0
+
 # convert back to sf
 parks <- st_as_sf(parks)
 neighborhoods <- st_as_sf(neighborhoods)
 
 # Output Data -------------------------------------------------------------
 
-if!(dir.exists("dat")) {
+if(!dir.exists("dat")) {
   dir.create("dat")
 }
 
+
+st_layers("dat/data.gpkg")
 # write to geopackage database
 write_sf(parks, "dat/data.gpkg", layer = "park")
 write_sf(neighborhoods, "dat/data.gpkg", layer = "neigh")
 write_sf(portland, "dat/data.gpkg", layer = "portland")
-
 # write indiv trees
+
 write_sf(park_tree, "dat/data.gpkg", layer = "park_trees")
 
